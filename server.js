02736d7faff5280a1fb3a7775d1c537d680273c1
yellingcookies <131363121+yellingcookies@users.noreply.github.com>
2023-04-20 07:11:19 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const stripe = require("stripe") (process.env.STRIPE_PRIVATE_KEY)
 const app = express();
 app.use(cors());
 app.use(express.json());
+const path = require("path")
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("build"))
+    app.get("*", (req, res) =>{
+        res.sendFile(path.resolve(__dirname, "build", "index.html"))
+    })
+}
 
 app.get("/", (req, res) => {
     res.send("Bienvenido a La Cocheerita");
